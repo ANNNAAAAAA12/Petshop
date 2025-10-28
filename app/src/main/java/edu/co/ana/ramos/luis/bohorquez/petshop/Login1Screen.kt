07a4f1e46dff6b3ec31de0a8c3f49fbe79e6b0f1
ANNNAAAAAA12 <aramos598@unab.edu.co>
2025-10-28 13:20:32 -0500

    Login maquetado

--- app/src/main/java/edu/co/ana/ramos/luis/bohorquez/petshop/Login1Screen.kt
+++ app/src/main/java/edu/co/ana/ramos/luis/bohorquez/petshop/Login1Screen.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -22,7 +27,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,8 +35,8 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun Login1Screen() {
     Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(24.dp)
+        .fillMaxWidth()
+        .fillMaxHeight()
         .background(Color.White),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
@@ -41,20 +45,62 @@ fun Login1Screen() {
         Image(
             painter = painterResource(id = R.drawable.logo_personas),
             contentDescription = "Logo PetMatch",
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.size(180.dp))
+            modifier = Modifier.size(200.dp),
+            alignment = Alignment.TopStart,
+            contentScale = ContentScale.Fit
+        )
 
+
+        //texto2
+        Text(modifier = Modifier.padding(24.dp, 1.dp),
+            text = "¿ Cómo quieres ingresar a la app ?",
+            fontSize = 26.sp,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
+Spacer(modifier = Modifier.size(66.dp))
         //boton 1
 
-        Button(onClick = { /* TODO: Acción de click aquí */ }, // <-- SINTAXIS CORRECTA
+        Button(onClick = { /* TODO: Acción de click aquí */ },
 
             modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp),
-            shape = RoundedCornerShape(12.dp),
+                .height(50.dp)
+                .width(190.dp),
+            shape = RoundedCornerShape(72.dp),
             colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFDFF6E0))
 
         ) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Login",
+                tint = Color.Black
+            )
+//texto
+
+            Text(
+                text = "Usuario",
+                fontSize = 16.sp,
+                color = Color.Black
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(20.dp))
+        //Boton2
+
+        Button(onClick = { /* TODO: Acción de click aquí */ },
+
+            modifier = Modifier
+                .height(50.dp)
+                .width(190.dp),
+            shape = RoundedCornerShape(72.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color((0xFFEBDCFB)))
+
+        ) {
+            Icon(
+                imageVector = Icons.Default.Home,
+                contentDescription = "Login",
+                tint = Color.Black
+            )
 //texto
 
             Text(
